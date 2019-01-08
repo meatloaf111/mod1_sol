5f02dd6ae7f4d0ea5c5c0bcb4417d321656240a9
meatloaf111 <meatloaf111jp@gmail.com>
2019-01-08 19:02:43 +0900

    Feature: LunchChecker

---
+++ app.js
@@ -0,0 +1,32 @@
+(function(){
+'use strict';
+
+angular.module('LunchCheck',[])
+
+.controller('LunchCheckController', LunchCheckContoller);
+
+LunchCheckContoller.$inject = ['$scope'];
+function LunchCheckContoller($scope){
+    $scope.LunchChcker = function(){
+        var lunchItems = 0;
+        if (!$scope.items){
+            $scope.message = "Please enter lunch item";
+
+        }else{
+            $scope.message = "";
+            var lunchInput = $scope.items.split(',');
+            var index;
+            for(index = 0; index< lunchInput.length; index++){
+                if(lunchInput[index].trim().length > 0){
+                    lunchItems++;
+                }
+            }
+            if(lunchItems <=3) $scope.message ="Enjoy";
+            else if (lunchItems > 3) $scope.message = "Too much";
+        }
+        $scope.lunchItems = lunchItems;
+    };
+
+}
+
+})();
